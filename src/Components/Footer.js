eba16a94a8fe852e88cd4964c730abdbde749451
NdunguSam01 <ndungu.muigai01@gmail.com>
2022-11-16 14:42:31 +0300

    Reverted back to original

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SocialIcon} from 'react-social-icons';
+const Footer=()=>
+{
+    return(
+        <div className="footer">
+            <SocialIcon url='https://twitter.com'  title='Twitter Page' target="_blank" rel="noopener noreferrer"></SocialIcon>
+            <SocialIcon url='https://facebook.com'  title='Facebook Page' target="_blank" rel="noopener noreferrer"></SocialIcon>
+            <SocialIcon url='https://instagram.com' title='Instagram Page' target="_blank" rel="noopener noreferrer"></SocialIcon>
+            <SocialIcon url='mailto:ndungu.muigai01@gmail.com' title='Email address' target="_blank" rel="noopener noreferrer"></SocialIcon>
+            <SocialIcon url='https://web.whatsapp.com/send?phone=+254707251073' title='WhatsApp contact' target="_blank" rel="noopener noreferrer"></SocialIcon>
+        </div>
+    )
+}
+export default Footer
---
+++ src/Components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SlArrowDown} from "react-icons/sl";
+const Nav=()=>
+{
+    return(
+        <div className="navbar">
+            <div className="search-container">
+                <input type="text" placeholder="Search"></input>
+                <button type="button" className="search">Search</button>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Account Details  <SlArrowDown/></button>
+                <div class="dropdown-content">
+                    <a href='./Accounts/MyAccount'>My Account</a>
+                      <a href='./Accounts/Orders'>Orders</a>
+                    <a href='./Accounts/Inbox'>Inbox</a>     
+                    <a href='./Accounts/Saved'>Saved Items</a>
+                </div>
+            </div> 
+            <a href='./Help'>Help</a>
+            <a href='./Cart'>Cart</a>
+        </div>
+    )
+}
+
+export default Nav
